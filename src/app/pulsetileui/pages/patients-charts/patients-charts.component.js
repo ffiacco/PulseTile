998001db87e0bb8ab9d020c5991803ed1ff3736b
JiafengLiao <mr.tornadochina@gmail.com>
2018-01-22 23:42:43 +0000

    dropdown list added

--- src/app/pulsetileui/pages/patients-charts/patients-charts.component.js
+++ src/app/pulsetileui/pages/patients-charts/patients-charts.component.js
@@ -242,3 +242,22 @@ const PatientsChartsComponent = {
 
 PatientsChartsController.$inject = ['$scope', '$state', '$window', 'patientsActions', '$ngRedux', 'serviceRequests', '$timeout', 'Patient', 'deviceDetector'];
 export default PatientsChartsComponent;
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
